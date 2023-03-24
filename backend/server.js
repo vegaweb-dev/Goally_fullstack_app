ea7e544e8b8dd50f97c572711bec604f9c0ca2ef
carlos <carlosjuniorrvega@hotmail.com>
2023-03-23 21:02:41 -0500

    feat:Start Server. CV

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('starting app');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const morgan = require('morgan');
+
+//Set port
+app.set('port', process.env.PORT || 5000);
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+//Routes
+app.get('/', (req, res) => {
+  res.json({ working: 'fine' });
+});
+//Database
+
+//Starting server
+app.listen(app.get('port'), () => {
+  console.log(`Server Running On Port ${app.get('port')}`);
+});
