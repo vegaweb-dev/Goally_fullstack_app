5b080e733cd44cadb3d516e5c3d01597188e2a03
carlos <carlosjuniorrvega@hotmail.com>
2023-04-24 21:38:05 -0500

    feat: added login functionality. CV

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,16 +17,14 @@ const registerUser = asyncHandler(async (req, res) => {
   // Check if user exists
 
   const userExists = await User.findOne({ email });
- 
 
   if (userExists) {
     res.status(400);
 
     throw new Error('User already exists');
   }
-  console.log('ya paso la condicion si existe');
-  const salt = await bcrypt.genSalt(10);
 
+  const salt = await bcrypt.genSalt(10);
   const hashedpassword = await bcrypt.hash(password, salt);
 
   //Create user
@@ -38,11 +36,11 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    console.log(user)
+    console.log(user);
     res.status(201).json({
       name: user.name,
       email: user.email,
-      _id:user.id
+      _id: user.id,
     });
   } else {
     res.status(400);
@@ -53,8 +51,23 @@ const registerUser = asyncHandler(async (req, res) => {
 //@description   Authenticate User
 //@route         Post api/users/login
 //@access        public
+
+//Check for user email
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'login user' });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  console.log(user,'aqui')
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      name: user.name,
+      email: user.email,
+      _id: user.id,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid Credentials');
+  }
 });
 
 //@description   Get User data
